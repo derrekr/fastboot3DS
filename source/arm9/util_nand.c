e5e256b51f1166ce1aed3453c529925568713ae2
profi200 <fd3194@gmx.de>
2016-03-21 19:16:48 +0100

    NAND dump/restore functions updated and adjusted for new code base.

--- source/arm9/util_nand.c
+++ source/arm9/util_nand.c
@@ -10,83 +10,81 @@
 #include "hid.h"
 #include "arm9/ndma.h"
 
-u64 getFreeSpace(const char *dev)
-{
-	DWORD freeClusters;
-	FATFS *fs;
 
-	if(f_getfree(dev, &freeClusters, &fs) != FR_OK)
+
+u64 getFreeSpace(const char *drive)
+{
+	FATFS *fs;
+	DWORD freeClusters;
+
+	if(f_getfree(drive, &freeClusters, &fs) != FR_OK)
 	{
-		printf("Failed to get free SD card space!\n");
+		printf("Failed to get free space for '%s'!\n", drive);
 		return 0;
 	}
 	return ((u64)(freeClusters * fs->csize)) * 512;
 }
 
-bool dumpNand(const char *filepath)
+bool dumpNand(const char *filePath)
 {
 	u32 nandSectorCnt = getMMCDevice(0)->total_size;
-	const u32 sectorBlkSize = 0x4000;
-	u32 curSector = 0;
+	u32 sectorBlkSize = 0x4000, curSector = 0;
+	u8 *buf = (u8*)FCRAM_BASE;
 	FIL file;
 	UINT bytesWritten;
 	u32 blockSize;
 	bool res = true;
-	u8 *buf = (u8 *) FCRAM_BASE;
 
-	if(getFreeSpace("sdmc") < (nandSectorCnt * 512))
+
+	if(getFreeSpace("sdmc:") < nandSectorCnt<<9)
 	{
 		printf("Not enough space on the SD card!\n");
 		return false;
 	}
-	
-	if(f_open(&file, filepath, FA_CREATE_ALWAYS | FA_WRITE) != FR_OK)
+	if(f_open(&file, filePath, FA_CREATE_ALWAYS | FA_WRITE) != FR_OK)
 	{
-		printf("Failed to create %s!\n", filepath);
+		printf("Failed to create '%s'!\n", filePath);
 		return false;
 	}
 
-	for(u32 i = 0; i <= nandSectorCnt / sectorBlkSize; i++)
+	while(curSector < nandSectorCnt)
 	{
 		blockSize = ((nandSectorCnt - curSector < sectorBlkSize) ? nandSectorCnt - curSector : sectorBlkSize);
 
-		if(blockSize > 0)
+		if(!dev_rawnand->read(curSector<<9, blockSize<<9, buf))
 		{
-			if(!dev_rawnand->read(curSector, blockSize, buf))
-			{
-				printf("\nFailed to read sector 0x%X!\n", (unsigned int)curSector);
-				res = false;
-				break;
-			}
-			if(f_write(&file, buf, blockSize * 512, &bytesWritten) != FR_OK)
-			{
-				printf("\nFailed to write to file!\n");
-				res = false;
-				break;
-			}
-
-			curSector += blockSize;
-			printf("\r%d%% (Sector 0x%X/0x%X)", (int)(curSector * 100 / nandSectorCnt), (unsigned int)curSector, (unsigned int)nandSectorCnt);
+			printf("\nFailed to read sector 0x%X!\n", (unsigned int)curSector);
+			res = false;
+			break;
 		}
+		if(f_write(&file, buf, blockSize<<9, &bytesWritten) != FR_OK || bytesWritten>>9 != blockSize)
+		{
+			printf("\nFailed to write to file!\n");
+			res = false;
+			break;
+		}
+
+		curSector += blockSize;
+		printf("\r%u%% (Sector 0x%X/0x%X)", (unsigned int)(curSector * 100 / nandSectorCnt), (unsigned int)curSector, (unsigned int)nandSectorCnt);
 	}
 
 	f_close(&file);
 	return res;
 }
 
-bool restoreNand(const char *filepath)
+bool restoreNand(const char *filePath)
 {
 	FIL file;
 	UINT bytesRead;
 	u32 blockSize;
 	bool res = true;
-	const u32 bufSize = 0x00800000;
-	u64 offset = 0;
-	u8 *buf = (u8 *) FCRAM_BASE;
+	u32 offset = 0;
+	u32 bufSize = 0x800000;
+	u8 *buf = (u8*)FCRAM_BASE;
 
 
 	FILINFO fileStat;
-	if(f_stat(filepath, &fileStat) != FR_OK)
+	if(f_stat(filePath, &fileStat) != FR_OK)
 	{
 		printf("Failed to get file status!\n");
 		return false;
@@ -97,35 +95,32 @@ bool restoreNand(const char *filepath)
 		return false;
 	}
 
-	if(f_open(&file, filepath, FA_READ) != FR_OK)
+	if(f_open(&file, filePath, FA_READ) != FR_OK)
 	{
-		printf("Failed to open '%s'!\n", filepath);
+		printf("Failed to open '%s'!\n", filePath);
 		return false;
 	}
 
-	u64 size = fileStat.fsize;
-	for(u32 i = 0; i <= size / bufSize; i++)
+	u32 size = fileStat.fsize;
+	while(offset < size)
 	{
 		blockSize = ((size - offset < bufSize) ? size - offset : bufSize);
 
-		if(blockSize > 0)
+		if(f_read(&file, buf, blockSize, &bytesRead) != FR_OK || bytesRead != blockSize)
 		{
-			if(f_read(&file, buf, blockSize, &bytesRead) != FR_OK)
-			{
-				printf("\nFailed to read from file!\n");
-				res = false;
-				break;
-			}
-			if(!dev_rawnand->write((u32)(offset>>9), blockSize>>9, buf))
-			{
-				printf("\nFailed to write sector 0x%X!\n", (unsigned int)offset>>9);
-				res = false;
-				break;
-			}
-
-			offset += blockSize;
-			printf("\r%d%% (Sector 0x%X/0x%X)", (int)(offset * 100 / size), (unsigned int)(offset>>9), (unsigned int)(size>>9));
+			printf("\nFailed to read from file!\n");
+			res = false;
+			break;
 		}
+		if(!dev_rawnand->write(offset, blockSize, buf))
+		{
+			printf("\nFailed to write sector 0x%X!\n", (unsigned int)offset>>9);
+			res = false;
+			break;
+		}
+
+		offset += blockSize;
+		printf("\r%d%% (Sector 0x%X/0x%X)", (unsigned int)((u64)offset * 100 / size), (unsigned int)offset>>9, (unsigned int)size>>9);
 	}
 
 	f_close(&file);
