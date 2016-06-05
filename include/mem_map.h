11fe9d99617b98cf3def873fa3ff435569971434
derrek <derrek.haxx@yahoo.com>
2016-06-05 16:37:32 +0200

    mem_map.h update

--- include/mem_map.h
+++ include/mem_map.h
@@ -4,8 +4,9 @@
 
 /* ARM9 MEM */
 #define A9_RAM_BASE       (0x08000000)
+#define A9_RAM_SIZE       (0x00100000)
 #define A9_HEAP_START     (0x08060000)
-#define A9_HEAP_END       (0x080C0000)
+#define A9_HEAP_END       (0x080F0000)
 #define	A9_STUB_ENTRY     (0x080FFE00)
 
 /* VRAM */
@@ -22,5 +23,4 @@
 /* FCRAM */
 #define FCRAM_BASE        (0x20000000)
 #define FCRAM_SIZE        (0x08000000) // this is larger for New3DS, but we don't care.
-#define FCRAM_HEAP_START  (FCRAM_BASE + 0x1000000)
-#define FCRAM_HEAP_END    (FCRAM_HEAP_START + 0x1000000)
+
