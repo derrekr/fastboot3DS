2d63b5018ebc8072bd1205ccc9ff64d57005a9ba
profi200 <fd3194@gmx.de>
2017-02-03 03:07:17 +0100

    Merge cpu.s back into start.s.

--- source/arm11/start.s
+++ source/arm11/start.s
@@ -1,3 +1,5 @@
+#include "mem_map.h"
+
 .arm
 .cpu mpcore
 .fpu vfpv2
@@ -5,40 +7,75 @@
 .global _start
 .global clearMem
 .global _init
+.global deinitCpu
 
 .type vectors STT_FUNC
 .type _start STT_FUNC
+.type stubExceptionVectors STT_FUNC
 .type clearMem STT_FUNC
+.type setupVfp STT_FUNC
 .type _init STT_FUNC
+.type deinitCpu STT_FUNC
 
-.extern initCpu
 .extern __bss_start__
 .extern __bss_end__
+.extern setupMmu
 .extern __libc_init_array
 .extern main
-.extern deinitCpu
 .extern firm_launch
 
 .section ".crt0"
 
 
 
-_vectors:
+vectors:
 	ldr pc, =_start            @ Reset vector
-	ldr pc, =(_vectors + 0x04) @ Undefined instruction vector
-	ldr pc, =(_vectors + 0x08) @ Software interrupt (SVC) vector
-	ldr pc, =(_vectors + 0x0C) @ Prefetch abort vector
-	ldr pc, =(_vectors + 0x10) @ Data abort vector
-	ldr pc, =(_vectors + 0x14) @ Reserved (unused) vector
-	ldr pc, =(_vectors + 0x18) @ Interrupt (IRQ) vector
-	ldr pc, =(_vectors + 0x1C) @ Fast interrupt (FIQ) vector
+	ldr pc, =(vectors + 0x04)  @ Undefined instruction vector
+	ldr pc, =(vectors + 0x08)  @ Software interrupt (SVC) vector
+	ldr pc, =(vectors + 0x0C)  @ Prefetch abort vector
+	ldr pc, =(vectors + 0x10)  @ Data abort vector
+	ldr pc, =(vectors + 0x14)  @ Reserved (unused) vector
+	ldr pc, =(vectors + 0x18)  @ Interrupt (IRQ) vector
+	ldr pc, =(vectors + 0x1C)  @ Fast interrupt (FIQ) vector
 .pool
 
 
 _start:
 	cpsid aif, #0x13           @ Disable all interrupts, SVC mode
 
-	bl initCpu
+	@ Control register:
+	@ [29] Force AP functionality             : disabled
+	@ [28] TEX remap                          : disabled
+	@ [27] NMFI bit                           : normal FIQ behavior
+	@ [25] CPSR E bit on taking an exception  : 0
+	@ [23] Extended page table configuration  : subpage AP bits enabled
+	@ [22] Unaligned data access              : disabled
+	@ [15] Disable loading TBIT               : disabled
+	@ [13] Vector select                      : 0x00000000
+	@ [12] Level one instruction cache        : disabled
+	@ [11] Program flow prediction            : disabled
+	@ [7]  Endianess                          : little
+	@ [2]  Level one data cache               : disabled
+	@ [1]  Strict data address alignment fault: disabled
+	@ [0]  MMU                                : disabled
+	ldr r0, =0x54078
+	mcr p15, 0, r0, c1, c0, 0   @ Write control register
+	mov r0, #0
+	mcr p15, 0, r0, c1, c0, 1   @ Write Auxiliary Control Register
+
+	mcr p15, 0, r0, c7, c5, 4   @ Flush Prefetch Buffer
+	mcr p15, 0, r0, c7, c7, 0   @ Invalidate Both Caches. Also flushes the branch target cache
+	mcr p15, 0, r0, c7, c10, 4  @ Data Synchronization Barrier
+
+	bl stubExceptionVectors     @ Stub the vectors in AXIWRAM bootrom vectors jump to
+
+	mov sp, #0                  @ SVC mode sp (Unused, aborts)
+	cpsid aif, #0x11            @ FIQ mode
+	mov sp, #0                  @ Not yet
+	cpsid aif, #0x12            @ IRQ mode
+	mov sp, #0                  @ Not yet
+	cpsid aif, #0x1F            @ System mode
+	ldr sp, =A11_STACK_END
 
 	@ Clear bss section
 	ldr r0, =__bss_start__
@@ -46,16 +83,37 @@ _start:
 	sub r1, r1, r0
 	bl clearMem
 
+	blx setupMmu
+	bl setupVfp
+	clrex
+	cpsie a
+
 	blx __libc_init_array      @ Initialize ctors and dtors
 
 	mov r0, #0                 @ argc
 	mov r1, #0                 @ argv
 	blx main
 	cmp r0, #0
-	bne .
+	bne fail_loop
 	bl deinitCpu
 	b firm_launch
+	fail_loop:
+		wfi
+		b fail_loop
 .pool
+
+
+#define MAKE_BRANCH(src, dst) (0xEA000000 | (((((dst) - (src)) >> 2) - 2) & 0xFFFFFF))
+
+stubExceptionVectors:
+	ldr r0, =A11_VECTORS_START
+	mov r1, #6
+	ldr r2, =MAKE_BRANCH(0, 0)  @ Endless loop
+	stubExceptionVectors_lp:
+		str r2, [r0], #8
+		subs r1, r1, #1
+		bne stubExceptionVectors_lp
+	bx lr
 
 
 @ void clearMem(u32 *adr, u32 size)
@@ -88,6 +146,50 @@ clearMem_check_zero:
 .pool
 
 
+setupVfp:
+	mov r0, #0
+	mov r1, #0xF00000           @ Give full access to cp10/11 in user and privileged mode
+	mcr p15, 0, r1, c1, c0, 2   @ Write Coprocessor Access Control Register
+	mcr p15, 0, r0, c7, c5, 4   @ Flush Prefetch Buffer
+	mov r1, #0x40000000         @ Clear exception bits and enable VFP11
+	mov r2, #0x3C00000          @ Round towards zero (RZ) mode, flush-to-zero mode, default NaN mode
+	fmxr fpexc, r1              @ Write Floating-point exception register
+	fmxr fpscr, r2              @ Write Floating-Point Status and Control Register
+	bx lr
+.pool
+
+
 _init:
 	bx lr
+.pool
+
+
+deinitCpu:
+	cpsid aif, #0x1F            @ System mode
+	mov r3, lr
+
+	bl stubExceptionVectors
+	bl flushDCache
+	mov r2, #0
+	mrc p15, 0, r0, c1, c0, 0   @ Read control register
+	ldr r1, =0x803807           @ Enable MMU, Strict data address alignment fault, D-Cache,
+	                            @ Program flow prediction, I-Cache, high exception vectors,
+	                            @ subpage AP bits disabled
+	bic r0, r0, r1
+	mcr p15, 0, r0, c1, c0, 0   @ Write control register
+	mrc p15, 0, r0, c1, c0, 1   @ Read Auxiliary Control Register
+	bic r0, r0, #0x6F           @ Return stack, Dynamic branch prediction, Static branch prediction,
+	                            @ Instruction folding, SMP mode: the CPU is taking part in coherency
+	                            @ and L1 parity checking
+	mcr p15, 0, r0, c1, c0, 1   @ Write Auxiliary Control Register
+
+	mcr p15, 0, r2, c7, c5, 4   @ Flush Prefetch Buffer
+	mcr p15, 0, r2, c7, c7, 0   @ Invalidate Both Caches. Also flushes the branch target cache
+	mcr p15, 0, r2, c7, c10, 4  @ Data Synchronization Barrier
+
+	@ Disable VFP11
+	fmxr fpscr, r2              @ Write Floating-Point Status and Control Register
+	fmxr fpexc, r2              @ Write Floating-point exception register
+	clrex
+	bx r3
 .pool
