3ee9d87a298ce1422cf20576aa231e4f8c9cc60a
profi200 <fd3194@gmx.de>
2017-11-14 00:37:17 +0100

    Make sure we can't write to non-FIRM partitions.

--- source/arm9/firmwriter.c
+++ source/arm9/firmwriter.c
@@ -30,29 +30,30 @@
 
 s32 writeFirmPartition(const char *const part)
 {
-	if(!dev_decnand->is_active()) return -1;
+	if(memcmp(part, "firm", 4) != 0) return -1;
+	if(!dev_decnand->is_active()) return -2;
 
 	size_t partInd, sector;
-	if(!partitionGetIndex(part, &partInd)) return -2;
-	if(!partitionGetSectorOffset(partInd, &sector)) return -3;
+	if(!partitionGetIndex(part, &partInd)) return -3;
+	if(!partitionGetSectorOffset(partInd, &sector)) return -4;
 
 	size_t firmSize;
-	if(!firm_size(&firmSize)) return -4;
+	if(!firm_size(&firmSize)) return -5;
 
 	u8 *firmBuf = (u8*)FIRM_LOAD_ADDR;
 	u8 *const cmpBuf = (u8*)malloc(FIRMWRITER_BLK_SIZE);
-	if(!cmpBuf) return -5;
+	if(!cmpBuf) return -6;
 
 	while(firmSize)
 	{
 		const u32 writeSize = min(firmSize, FIRMWRITER_BLK_SIZE);
 
-		if(!dev_decnand->write_sector(sector, writeSize>>9, firmBuf)) return -6;
-		if(!dev_decnand->read_sector(sector, writeSize>>9, cmpBuf)) return -6;
+		if(!dev_decnand->write_sector(sector, writeSize>>9, firmBuf)) return -7;
+		if(!dev_decnand->read_sector(sector, writeSize>>9, cmpBuf)) return -7;
 		if(memcmp(firmBuf, cmpBuf, writeSize) != 0)
 		{
 			free(cmpBuf);
-			return -7;
+			return -8;
 		}
 
 		sector += writeSize>>9;
