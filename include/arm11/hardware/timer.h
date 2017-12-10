a90c0dd8768fbc94a0025e9fa36813c7d696fd46
profi200 <fd3194@gmx.de>
2017-12-10 15:07:44 +0100

    ARM11: Timer prescalers start at 1.

--- include/arm11/hardware/timer.h
+++ include/arm11/hardware/timer.h
@@ -29,7 +29,7 @@
 #define TIMER_IRQ_ENABLE   (1u<<2)
 
 // p is the prescaler value and n the frequence
-#define TIMER_FREQ(p, n)   (TIMER_BASE_FREQ / 2 / ((p) + 1) / (n))
+#define TIMER_FREQ(p, n)   (TIMER_BASE_FREQ / 2 / (p) / (n))
 
 
 
